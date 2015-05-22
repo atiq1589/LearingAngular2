a8a8bfa3389f8227bdbe600f7a0c26ab8b339f02
atiq1589 <atiqul.islam1589@gmail.com>
2015-05-23 00:48:42 +0600

    add todo

--- todo.ts
+++ todo.ts
@@ -1,15 +1,26 @@
 </// <reference path="typings/angular2/angular2.d.ts">
 
-import {Component, View, bootstrap} from 'angular2/angular2';
+import {Component, View, bootstrap, For} from 'angular2/angular2';
 
-
+@Component({selector:'todo-list'})
+@View({
+	templateUrl: 'todo-partial.html',
+	directives: [For]
+})
 class TodoList{
 	todos: Array<string>;
 	constructor(){
-		this.todos = ["Bike riding", "Colse word"];
+		this.todos = ["Bike riding", "Close word"];
 	}
 	addTodo(todo: string){
 		this.todos.push(todo);
+	}
+	doneTyping($event){
+		$event.getPreventDefault();
+		if($event.which === 13){
+			this.addTodo($event.target.values);
+			$event.target.value = null;
+		}
 	}
 }
 
